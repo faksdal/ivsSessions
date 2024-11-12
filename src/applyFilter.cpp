4cd91e053dd85b79a76cdca96711cc6dc6400e46
Jon Leithe <jon@jonleithe.no>
2024-11-12 16:34:16 +0100

    Initial commit

--- src/applyFilter.cpp
+++ src/applyFilter.cpp
@@ -19,13 +19,24 @@ void ivsSessions::applyFilter(void)
 		print(0, row++, d_colors.str_default_color + "                                                                                                                                             ");
 	}
 
-	switch(vl_filter[getActiveFilterIndex()].ft){
-		case FilterTypes::SESSION_TYPE:		break;
-		case FilterTypes::DATE:				break;
-		case FilterTypes::SESSION_CODE:		break;
-		case FilterTypes::STATIONS:			break;
 
-	}
+	for(unsigned long i = 0; i < vl_sessionList.size(); i++){
+
+		switch(vl_filter[getActiveFilterIndex()].ft){
+
+			case FilterTypes::SESSION_TYPE:		if(vl_sessionList[i].str_sessionType.find(vl_filter[getActiveFilterIndex()].str_filterText) == std::string::npos)
+													vl_filter[getActiveFilterIndex()].str_filterText)
+												break;
+			case FilterTypes::DATE:				//
+												break;
+			case FilterTypes::SESSION_CODE:		//
+												break;
+			case FilterTypes::STATIONS:			//
+												break;
+
+		} // switch(vl_filter[getActiveFilterIndex()].ft)
+
+	} // for(unsigned long i = 0; i < vl_sessionList.size(); i++)
 
 	//printSession(d_bound.i_list_start_row, 0, d_colors.str_list_highlighted_color);
 	//updateListTracking(0, 0, d_bound.i_list_start_row);
